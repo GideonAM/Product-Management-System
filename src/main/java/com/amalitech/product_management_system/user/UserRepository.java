574062fa8c2ed29bc04387986a07772777afac2e
Gideon Agbosu <gideon.agbosu@amalitech.com>
2024-12-09 08:54:05 +0000

    Added native sql query

--- src/main/java/com/amalitech/product_management_system/user/UserRepository.java
+++ src/main/java/com/amalitech/product_management_system/user/UserRepository.java
@@ -1,9 +1,16 @@
 package com.amalitech.product_management_system.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByEmail(String userEmail);
+    @Query(value = """
+            SELECT *
+            FROM _user u
+            WHERE u.email = ?1
+            """, nativeQuery = true)
+    Optional<User> findByEmail(@Param("email") String userEmail);
 }

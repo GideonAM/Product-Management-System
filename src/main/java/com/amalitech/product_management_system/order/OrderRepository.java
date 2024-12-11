cbb278c05331a315484ce88f3a2c06c67504090d
Gideon Agbosu <gideon.agbosu@amalitech.com>
2024-12-11 19:11:25 +0000

    Modified OrderRepository setup

--- src/main/java/com/amalitech/product_management_system/order/OrderRepository.java
+++ src/main/java/com/amalitech/product_management_system/order/OrderRepository.java
@@ -2,5 +2,5 @@ package com.amalitech.product_management_system.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, PaymentMethod> {
+public interface OrderRepository extends JpaRepository<Order, String> {
 }
